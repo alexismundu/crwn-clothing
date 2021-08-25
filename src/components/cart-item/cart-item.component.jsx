f850c78339085426fd846b4ad53ad07d4aca2729
Alexis Munoz <alexismun53@gmail.com>
2021-08-24 21:47:03 -0500

    create and implement styled components for CartItem component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 
-import "./cart-item.styles.scss";
+import {
+  CartItemContainer,
+  ItemImage,
+  ItemDetailsContainer,
+  ItemName,
+} from "./cart-item.styles";
 
 const CartItem = ({ item: { name, price, imageUrl, quantity } }) => (
-  <div className="cart-item">
-    <img src={imageUrl} alt="item" />
-    <div className="item-details">
-      <span className="name">{name}</span>
-      <span className="price">
+  <CartItemContainer>
+    <ItemImage src={imageUrl} alt="item" />
+    <ItemDetailsContainer>
+      <ItemName>{name}</ItemName>
+      <span>
         {quantity} x {price}
       </span>
-    </div>
-  </div>
+    </ItemDetailsContainer>
+  </CartItemContainer>
 );
 
 export default CartItem;
