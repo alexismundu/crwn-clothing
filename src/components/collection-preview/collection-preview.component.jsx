1448eb161168193048715ea9b2b66d880b56196f
Alexis Munoz <alexismun53@gmail.com>
2021-08-24 22:07:01 -0500

    create and implement styled components for CollectionPreview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 
-import "./collection-preview.styles.scss";
 import CollectionItem from "../collection-item";
 
+import {
+  CollectionPreviewContainer,
+  TitleContainer,
+  Preview,
+} from "./collection-preview.styles";
+
 const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
+  <CollectionPreviewContainer>
+    <TitleContainer>{title.toUpperCase()}</TitleContainer>
+    <Preview>
       {items
         .filter((item, i) => i < 4)
         .map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-    </div>
-  </div>
+    </Preview>
+  </CollectionPreviewContainer>
 );
 
 export default CollectionPreview;
